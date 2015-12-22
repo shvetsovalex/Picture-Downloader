5b67819a3c5c3c7cfe93fc7917578dabefa0f851
Александр <shvetsovalex007@gmail.com>
2015-12-22 21:13:24 +0400

    Загрузка картинки через AsynkTaskLoader и BroadcastReceiver

--- app/src/main/java/com/example/picture_downloader/ImageLoader.java
+++ app/src/main/java/com/example/picture_downloader/ImageLoader.java
@@ -1,63 +1,62 @@
 package com.example.picture_downloader;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.v4.content.AsyncTaskLoader;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLEncoder;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.content.AsyncTaskLoader;
-
-public class ImageLoader extends AsyncTaskLoader<Void> {
+public class ImageLoader extends AsyncTaskLoader<Integer> {
     protected final static int DOWNLOADING_STATUS = 0;
     protected final static int DOWNLOADED_STATUS = 1;
     protected final static int IDLE_STATUS = 2;
     protected final static int ERROR_STATUS = 3;
     protected final static int MAX_PROGRESS = 100;
+    protected final static String ACTION_PROGRESS = "com.example.picture_downloader.Status";
 
-    protected final static String fileName = "pic.png";
+    protected final static String fileName = "pic.jpg";
 
     private String url;
     private int progress;
     private int status;
     private Handler handler;
     private String file_path;
+    private Context context;
 
 
     public ImageLoader(Context context) {
         super(context);
     }
 
-    public ImageLoader(Context context, String url, String file_path, Handler handler) {
+    public ImageLoader(Context context, String url, String file_path) {
         super(context);
         this.url = url;
         this.file_path = file_path;
-        status = IDLE_STATUS;
-        this.handler = handler;
+        this.status = IDLE_STATUS;
+        this.context = context;
+
     }
 
     @Override
-    public Void loadInBackground() {
+    public Integer loadInBackground() {
         status = DOWNLOADING_STATUS;
-
+        System.out.println(status + "start loading");
         try {
             URL url = new URL(this.url);
             URLConnection connection = url.openConnection();
             connection.connect();
             int lengthOfFile = connection.getContentLength();
-            System.out.println(lengthOfFile);
+            System.out.println(lengthOfFile); //log
             InputStream input = new BufferedInputStream(url.openStream(), 8192);
 
             OutputStream output = new FileOutputStream(new File(Environment.getExternalStorageDirectory() + "/" + this.fileName));
@@ -70,7 +69,7 @@ public class ImageLoader extends AsyncTaskLoader<Void> {
                 total += count;
                 progress = (int)((total*100)/lengthOfFile);
                 output.write(data, 0, count);
-                publishProgress();
+                publishStatus();
             }
 
             output.flush();
@@ -78,17 +77,13 @@ public class ImageLoader extends AsyncTaskLoader<Void> {
             input.close();
 
         } catch (IOException e) {
-            handler.sendEmptyMessage(ERROR_STATUS);
+            publishStatus();
             return null;
         }
 
-        handler.sendEmptyMessage(DOWNLOADED_STATUS);
         status = DOWNLOADED_STATUS;
+        publishStatus();
         return null;
-    }
-
-    public void setHandler(Handler handler) {
-        this.handler = handler;
     }
 
     public int getStatus() {
@@ -99,7 +94,9 @@ public class ImageLoader extends AsyncTaskLoader<Void> {
         return progress;
     }
 
-    private void publishProgress() {
-        handler.sendEmptyMessage(DOWNLOADING_STATUS);
+    private void publishStatus() {
+        Intent intent = new Intent(ACTION_PROGRESS);
+        intent.putExtra("status", status);
+        context.sendBroadcast(intent);
     }
 }
--- app/src/main/java/com/example/picture_downloader/ImageLoaderActivity.java
+++ app/src/main/java/com/example/picture_downloader/ImageLoaderActivity.java
@@ -1,13 +1,13 @@
 package com.example.picture_downloader;
 
 
-import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBarActivity;
@@ -17,89 +17,26 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import com.example.picture_downloader.ImageLoader;
 
-public class ImageLoaderActivity extends ActionBarActivity implements LoaderManager.LoaderCallbacks<Void> {
-    private Loader imLoad;
+public class ImageLoaderActivity extends ActionBarActivity implements LoaderManager.LoaderCallbacks<Integer> {
+    private Loader imageLoader;
     private static int LOADER_IMAGE_ID = 1;
     private Button btnLoad;
     private TextView statusLabel;
     private ProgressBar progressBar;
+    private ProgressReceiver progressReceiver;
     private static String url;
-    private Handler handler;
-    private long status;
+    private int status;
     private int downloaded;
+    private IntentFilter intentFilter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-                status = msg.what;
-                switch (msg.what) {
-                    case ImageLoader.IDLE_STATUS: {
-                        btnLoad.setEnabled(true);
-                        btnLoad.setText(R.string.download);
-
-                        progressBar.setVisibility(View.GONE);
-                        progressBar.setMax(ImageLoader.MAX_PROGRESS);
-                        progressBar.setProgress(0);
-
-                        statusLabel.setText(R.string.status_idle);
-                    }
-                    break;
-                    case ImageLoader.DOWNLOADING_STATUS: {
-
-                        int progress = ((ImageLoader) imLoad).getProgress();
-
-                        progressBar.setVisibility(View.VISIBLE);
-                        progressBar.setProgress(progress);
-
-                        btnLoad.setEnabled(false);
-
-                        statusLabel.setText(R.string.status_downloading);
-                    }
-                    break;
-
-                    case ImageLoader.DOWNLOADED_STATUS: {
-                        progressBar.setVisibility(View.GONE);
-
-                        btnLoad.setEnabled(true);
-                        btnLoad.setText(R.string.downloaded);
-
-                        statusLabel.setText(R.string.status_downloaded);
-
-                    }
-                    break;
-
-                    case ImageLoader.ERROR_STATUS: {
-                        Toast.makeText(ImageLoaderActivity.this,
-                                R.string.loading_error, Toast.LENGTH_SHORT)
-                                .show();
-
-                        imLoad = getSupportLoaderManager().restartLoader(LOADER_IMAGE_ID, new Bundle(), ImageLoaderActivity.this);
-
-                        this.sendEmptyMessage(ImageLoader.IDLE_STATUS);
-                    }
-                    break;
-                }
-            }
-
-        };
-
-        btnLoad = (Button)findViewById(R.id.btnLoad);
+        btnLoad = (Button) findViewById(R.id.btnLoad);
 
         statusLabel = (TextView) findViewById(R.id.status_label);
         progressBar = (ProgressBar) findViewById(R.id.progress);
@@ -107,68 +44,131 @@ public class ImageLoaderActivity extends ActionBarActivity implements LoaderMana
 
         this.url = getString(R.string.url);
 
-        imLoad = getSupportLoaderManager().getLoader(LOADER_IMAGE_ID);
-        if (imLoad == null) {
+        imageLoader = getSupportLoaderManager().getLoader(LOADER_IMAGE_ID);
+        if (imageLoader == null) {
             getSupportLoaderManager().initLoader(LOADER_IMAGE_ID, new Bundle(), ImageLoaderActivity.this);
-            ((ImageLoader)imLoad).setHandler(handler);
         }
-        else
-            ((ImageLoader)imLoad).setHandler(handler);
-        if(savedInstanceState != null && savedInstanceState.getInt("downloaded") == 1) {
+
+        if (savedInstanceState != null && savedInstanceState.getInt("downloaded") == 1) {
+            downloaded = 1;
             status = ImageLoader.DOWNLOADED_STATUS;
             statusLabel.setText(R.string.status_downloaded);
             btnLoad.setText(R.string.downloaded);
-        }
-        else
+        } else
             status = ImageLoader.IDLE_STATUS;
-        if (imLoad == null) {
-            handler.sendEmptyMessage(ImageLoader.ERROR_STATUS);
-            btnLoad.setEnabled(false);
-            return;
-        }
-
+        progressReceiver = new ProgressReceiver();
+        intentFilter = new IntentFilter(ImageLoader.ACTION_PROGRESS);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.registerReceiver(progressReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(progressReceiver);
+    }
 
     @Override
     public Loader onCreateLoader(int id, Bundle args) {
-        imLoad = new ImageLoader(this, url, "", handler);
+        imageLoader = new ImageLoader(this, url, "");
         System.out.println("create loader");
-        return imLoad;
+        return imageLoader;
     }
 
-
     @Override
-    public void onLoadFinished(Loader<Void> loader, Void data) {
-        System.out.println("onLoadFinished for loader " + loader.hashCode());
+    public void onLoadFinished(Loader<Integer> loader, Integer data) {
         progressBar.setVisibility(ProgressBar.INVISIBLE);
     }
 
-
     @Override
-    public void onLoaderReset(Loader<Void> loader) {
+    public void onLoaderReset(Loader<Integer> loader) {
         System.out.println("onLoaderReset for loader " + loader.hashCode());
     }
 
     public void loadImageClick(View v) {
-        System.out.println("onclick hashcode: " + handler.hashCode());
-        if(status == ImageLoader.IDLE_STATUS) {
-            imLoad.forceLoad();
+        System.out.println("onclick hashcode: ");
+        if (status == ImageLoader.IDLE_STATUS) {
+            imageLoader.forceLoad();
             downloaded = 1;
+            status = ImageLoader.DOWNLOADED_STATUS;
         }
-        if(status == ImageLoader.DOWNLOADED_STATUS) {
+        else if (status == ImageLoader.DOWNLOADED_STATUS) {
             Intent intent = new Intent();
             intent.setAction(Intent.ACTION_VIEW);
-            intent.setDataAndType(Uri.parse("file://" + Environment.getExternalStorageDirectory() + "/pic.png"), "image/*");
+            intent.setDataAndType(Uri.parse("file://" + Environment.getExternalStorageDirectory() + "/pic.jpg"), "image/*");
             startActivity(intent);
 
         }
     }
 
+    private void updateStatus(int status) {
+        switch (status) {
+            case ImageLoader.IDLE_STATUS: {
+                btnLoad.setEnabled(true);
+                btnLoad.setText(R.string.download);
+
+                progressBar.setVisibility(View.GONE);
+                progressBar.setMax(ImageLoader.MAX_PROGRESS);
+                progressBar.setProgress(0);
+
+                statusLabel.setText(R.string.status_idle);
+            }
+            break;
+            case ImageLoader.DOWNLOADING_STATUS: {
+
+                int progress = ((ImageLoader) imageLoader).getProgress();
+
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setProgress(progress);
+                System.out.println(progress);
+                btnLoad.setEnabled(false);
+
+                statusLabel.setText(R.string.status_downloading);
+            }
+            break;
+
+            case ImageLoader.DOWNLOADED_STATUS: {
+                progressBar.setVisibility(View.GONE);
+
+                btnLoad.setEnabled(true);
+                btnLoad.setText(R.string.downloaded);
+
+                statusLabel.setText(R.string.status_downloaded);
+
+            }
+            break;
+
+            case ImageLoader.ERROR_STATUS: {
+                Toast.makeText(ImageLoaderActivity.this,
+                        R.string.loading_error, Toast.LENGTH_SHORT)
+                        .show();
+
+                imageLoader = getSupportLoaderManager().restartLoader(LOADER_IMAGE_ID, new Bundle(), ImageLoaderActivity.this);
+                this.status = ImageLoader.IDLE_STATUS;
+            }
+            break;
+        }
+    }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("downloaded", downloaded);
     }
-}
 
+    class ProgressReceiver extends BroadcastReceiver {
+        private int status;
+        public ProgressReceiver() {
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            status = intent.getIntExtra("status", status);
+            updateStatus(status);
+        }
+
+    }
+}
